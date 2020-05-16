860b01a3cb42510e505437824b2a31d3e31d0858
Mieszko Sabo <ms406322@students.mimuw.edu.pl>
2020-05-16 23:11:39 +0200

    pusta klasa do robienia requestów i eventlistnery w main

--- IOApka/app/src/main/java/com/example/ioapka/MainActivity.java
+++ IOApka/app/src/main/java/com/example/ioapka/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.ioapka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button fridgeButton = findViewById(R.id.fridgeButton);
+        // wygląda na to, że takie coś sprawia, że kliknięcia idą do tego onClick niżej
+        fridgeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // każdy button może być tutaj w odzielnym case'ie i pyk
+        switch (v.getId()) {
+            case R.id.fridgeButton:
+                Toast.makeText(this, "Button clicked",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
