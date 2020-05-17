59b92ea113806530ed743f102835eeb8c90cf0b2
Mieszko Sabo <ms406322@students.mimuw.edu.pl>
2020-05-17 11:52:10 +0200

    Apka umie już komunikować się z serverem... jako tako

--- IOApka/app/src/main/java/com/example/ioapka/MainActivity.java
+++ IOApka/app/src/main/java/com/example/ioapka/MainActivity.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewResult; // pole tekstowe do testowania http responsów
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextViewResult = findViewById(R.id.resTest);
 
         Button fridgeButton = findViewById(R.id.fridgeButton);
         // wygląda na to, że takie coś sprawia, że kliknięcia idą do tego onClick niżej
@@ -24,7 +34,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // każdy button może być tutaj w odzielnym case'ie i pyk
         switch (v.getId()) {
             case R.id.fridgeButton:
-                Toast.makeText(this, "Button clicked",Toast.LENGTH_SHORT).show();
+                // Należy napisać swój Callback zawierający onFailure i OnResponse
+                // tak jak niżej. W onResponse pisze się co chcemy zrobić z responsem.
+                // TODO: fajniej by było jakby serverCommunication returnował JSON'a z odp np
+                // TODO: a nie że trzeba pisać callbacka, ale na razie nie wiem jak to zrobić
+                Callback myCallback = new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mTextViewResult.setText(myResponse);
+                                }
+                            });
+                        }
+                    }
+                };
+                // Potem tworzymy obiekt ServerCommunication i możemy użyć np metody lookupEAN
+                ServerCommunication sc = new ServerCommunication();
+                sc.lookupEAN("111111111111", myCallback);
                 break;
         }
     }
--- IOApka/app/src/main/java/com/example/ioapka/ServerCommunication.java
+++ IOApka/app/src/main/java/com/example/ioapka/ServerCommunication.java
@@ -1,7 +1,24 @@
 package com.example.ioapka;
 
 
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 public class ServerCommunication {
-// TODO
+
+    private OkHttpClient client;
+
+    public ServerCommunication() {
+        client = new OkHttpClient();
+    }
+
+    public void lookupEAN(String EAN, Callback cb) {
+        System.out.println("jestem");
+        String url = "http://10.0.2.2:8080/lookupEAN=" + EAN;
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(cb);
+    }
 
 }
