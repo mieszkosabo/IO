1bf8985e6dcfa16bdf90e1b41850113cd4784f70
Lucas <lu373363@gitlab.uw.edu.pl>
2020-05-17 21:01:14 +0200

    Dodanie ekranu z dodawaniem przepisu vol.2

--- IOApka/app/src/main/java/com/example/ioapka/RecipesMenu.java
+++ IOApka/app/src/main/java/com/example/ioapka/RecipesMenu.java
@@ -3,6 +3,7 @@ package com.example.ioapka;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RecipesMenu extends AppCompatActivity {
 
     private ImageButton backToMain;
+    private Button AddRecipe;
     private Object MainActivity;
 
     @Override
@@ -24,10 +26,23 @@ public class RecipesMenu extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        AddRecipe = findViewById(R.id.AddRecipe);
+        AddRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddingRecipe();
+            }
+        });
     }
 
     public void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openAddingRecipe() {
+        Intent intent = new Intent(this, AddRecipe.class);
         startActivity(intent);
     }
 }
