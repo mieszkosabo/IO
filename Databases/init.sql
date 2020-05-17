fbc0ad29e773959875b1516448120db9079b3670
kamilkox <57043007+kamilkox@users.noreply.github.com>
2020-05-17 23:31:05 +0200

    categories

--- Databases/init.sql
+++ Databases/init.sql
@@ -229,13 +229,13 @@ DELIMITER ;
 
                                       /********LOADY*******/
 
-/*
+
 LOAD DATA LOCAL INFILE 'csv/category.csv'
     INTO TABLE CATEGORY
     FIELDS TERMINATED BY ';'
     LINES TERMINATED BY '\n'
     IGNORE 1 LINES;
-
+/*
 LOAD DATA LOCAL INFILE 'csv/dish.csv'
     INTO TABLE DISH
     FIELDS TERMINATED BY ';'
