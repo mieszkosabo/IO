9aa685bcbbb92b36c4d65909aefbde91ec8307e7
Lucas <lu373363@gitlab.uw.edu.pl>
2020-05-18 21:36:13 +0200

    Dodanie kolejnego kroku w dodawaniu przepisu

--- IOApka/app/src/main/java/com/example/ioapka/AddRecipe.java
+++ IOApka/app/src/main/java/com/example/ioapka/AddRecipe.java
@@ -1,11 +1,16 @@
 package com.example.ioapka;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.CheckBox;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,6 +20,8 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
+import static com.example.ioapka.R.drawable.button_background2;
+
 public class AddRecipe extends AppCompatActivity {
 
     private Button Next;
@@ -22,6 +29,8 @@ public class AddRecipe extends AppCompatActivity {
     private Button MakePhoto;
     private Button Prev;
     private Integer counter;
+    private ArrayList<String> Tags;
+    public ConstraintLayout cl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +50,14 @@ public class AddRecipe extends AppCompatActivity {
             }
         });
 
+        cl = findViewById(R.id.MyLayout);
     }
 
     public void OnNext() {
         if (counter == 0)  {
             Prev.setVisibility(View.VISIBLE);
             AddPhoto.setVisibility(View.INVISIBLE);
-            MakePhoto.setVisibility((View.INVISIBLE));
+            MakePhoto.setVisibility(View.INVISIBLE);
 
             Callback callback = new Callback() {
                 @Override
@@ -60,12 +70,74 @@ public class AddRecipe extends AppCompatActivity {
                     if (response.isSuccessful()) {
                         String myResponse = response.body().string();
                         myResponse = myResponse.substring(1, myResponse.length() - 1);
-                        final ArrayList<String> categories= new ArrayList<String>(Arrays.asList(myResponse.split(",")));
-                        final TextView text = findViewById(R.id.textView);
+                        Tags = new ArrayList<String>(Arrays.asList(myResponse.split(",")));
+                        final ArrayList<CheckBox> cbs = new ArrayList<CheckBox>();
                         AddRecipe.this.runOnUiThread(new Runnable() {
+                            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
                             @Override
                             public void run() {
-                                text.setText(categories.get(0).substring(1, categories.get(0).length() - 1));
+                                for (int i = 0; i < Tags.size(); i++) {
+                                    if (i == 0) {
+                                        ConstraintSet set = new ConstraintSet();
+                                        CheckBox cb = new CheckBox(AddRecipe.this);
+                                        cb.setId(View.generateViewId());
+                                        cl.addView(cb);
+                                        set.clone(cl);
+                                        set.connect(cb.getId(), ConstraintSet.LEFT, cl.getId(), ConstraintSet.LEFT, 120);
+                                        set.connect(cb.getId(), ConstraintSet.TOP, findViewById(R.id.toolbarAdding).getId(), ConstraintSet.BOTTOM, 40);
+                                        set.applyTo(cl);
+                                        cb.setText(Tags.get(i).substring(1, Tags.get(i).length() - 1));
+                                        cb.setBackgroundResource(button_background2);
+                                        cb.setTextColor(Color.parseColor("#FFF6CE"));
+                                        cb.setPadding(4, 4, 12, 4);
+                                        cbs.add(cb);
+                                    }
+                                    if (i > 0 && i < 10) {
+                                        ConstraintSet set = new ConstraintSet();
+                                        CheckBox cb = new CheckBox(AddRecipe.this);
+                                        cb.setId(View.generateViewId());
+                                        cl.addView(cb);
+                                        set.clone(cl);
+                                        set.connect(cb.getId(), ConstraintSet.LEFT, cl.getId(), ConstraintSet.LEFT, 120);
+                                        set.connect(cb.getId(), ConstraintSet.TOP, cbs.get(i-1).getId(), ConstraintSet.BOTTOM, 20);
+                                        set.applyTo(cl);
+                                        cb.setText(Tags.get(i).substring(1, Tags.get(i).length() - 1));
+                                        cb.setBackgroundResource(button_background2);
+                                        cb.setTextColor(Color.parseColor("#FFF6CE"));
+                                        cb.setPadding(4, 4, 12, 4);
+                                        cbs.add(cb);
+                                    }
+                                    if (i == 10) {
+                                        ConstraintSet set = new ConstraintSet();
+                                        CheckBox cb = new CheckBox(AddRecipe.this);
+                                        cb.setId(View.generateViewId());
+                                        cl.addView(cb);
+                                        set.clone(cl);
+                                        set.connect(cb.getId(), ConstraintSet.RIGHT, cl.getId(), ConstraintSet.RIGHT, 120);
+                                        set.connect(cb.getId(), ConstraintSet.TOP, findViewById(R.id.toolbarAdding).getId(), ConstraintSet.BOTTOM, 40);
+                                        set.applyTo(cl);
+                                        cb.setText(Tags.get(i).substring(1, Tags.get(i).length() - 1));
+                                        cb.setBackgroundResource(button_background2);
+                                        cb.setTextColor(Color.parseColor("#FFF6CE"));
+                                        cb.setPadding(4, 4, 12, 4);
+                                        cbs.add(cb);
+                                    }
+                                    if (i > 10) {
+                                        ConstraintSet set = new ConstraintSet();
+                                        CheckBox cb = new CheckBox(AddRecipe.this);
+                                        cb.setId(View.generateViewId());
+                                        cl.addView(cb);
+                                        set.clone(cl);
+                                        set.connect(cb.getId(), ConstraintSet.RIGHT, cl.getId(), ConstraintSet.RIGHT, 120);
+                                        set.connect(cb.getId(), ConstraintSet.TOP, cbs.get(i-1).getId(), ConstraintSet.BOTTOM, 20);
+                                        set.applyTo(cl);
+                                        cb.setText(Tags.get(i).substring(1, Tags.get(i).length() - 1));
+                                        cb.setBackgroundResource(button_background2);
+                                        cb.setTextColor(Color.parseColor("#FFF6CE"));
+                                        cb.setPadding(4, 4, 12, 4);
+                                        cbs.add(cb);
+                                    }
+                                }
                             }
                         });
                     }
@@ -73,7 +145,7 @@ public class AddRecipe extends AppCompatActivity {
             };
             ServerCommunication sc = new ServerCommunication();
             sc.lookupCategory(callback);
+            counter++;
         }
-
     }
 }
